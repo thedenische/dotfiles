b1befec23e874ab0e2524768197845fb30422834
DenisChe <the.denis.che@gmail.com>
2026-02-10 11:15:42 +0100

    fix: [nvim] ufo lsp capabilities with vim.lsp

--- nvim/.config/nvim/lua/denische/plugins/editor/ufo/config.lua
+++ nvim/.config/nvim/lua/denische/plugins/editor/ufo/config.lua
@@ -1,22 +1,22 @@
 -- folding settings
 vim.o.foldcolumn     = '1' -- '0' is not bad
+vim.o.foldlevel      = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable     = true
 -- keymaps
 require('denische.plugins.editor.ufo.keymaps')
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true
-}
+local capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), {
+    textDocument = {
+        foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+        }
+    }
+})
 
-local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-    require('lspconfig')[ls].setup({
-        capabilities = capabilities
-        -- you can add other fields for setting up lsp server in this table
-    })
-end
+vim.lsp.config("*", {
+    capabilities = capabilities
+})
 
 require('ufo').setup()
