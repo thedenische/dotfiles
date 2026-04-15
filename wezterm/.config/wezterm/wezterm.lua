2d2f291357d24f4683dc14184291995938b14231
DenisChe <the.denis.che@gmail.com>
2026-04-15 12:08:23 +0200

    feat: [wezterm] impreve line hieght

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ config.native_macos_fullscreen_mode = true
 -- Fonts
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 14.0
+config.line_height = 1.1
 
 -- Colorscheme
 config.color_scheme = 'Everforest Dark (Gogh)'
